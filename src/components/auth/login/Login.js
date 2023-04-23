741363005764e4c32a830c1b043f40f1f0d02025
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2023-04-23 19:51:07 +0300

    add basic login and register forms

--- src/components/auth/login/Login.js
+++ src/components/auth/login/Login.js
@@ -1,3 +1,26 @@
 export const Login = () => {
-    <h1>Login</h1>;
+    return (
+        <div className="register-form-wraper">
+            <h1>Sign In</h1>
+            <form>
+                <div>
+                    <label htmlFor="email-sign-in">Email:</label>
+                    <input name="email" id="email-sign-in" type="text" />
+                </div>
+
+                <div>
+                    <label htmlFor="password-sign-in">Password:</label>
+                    <input
+                        name="password"
+                        id="password-sign-in"
+                        type="password"
+                    />
+                </div>
+
+                <div>
+                    <input type="submit" value="Login" />
+                </div>
+            </form>
+        </div>
+    );
 };
--- src/components/auth/register/Register.js
+++ src/components/auth/register/Register.js
@@ -1,3 +1,72 @@
 export const Register = () => {
-    return <h1>Register</h1>;
+    return (
+        <div className="register-form-wraper">
+            <h1>Sign Up</h1>
+            <form>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        name="username"
+                        id="username"
+                        type="text"
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="password-sign-up">Password:</label>
+                    <input
+                        name="password"
+                        id="password-sign-up"
+                        type="password"
+
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email-sign-up">Email:</label>
+                    <input
+                        name="email"
+                        id="email-sign-up"
+                        type="text"
+
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="age">Age:</label>
+                    <input
+                        name="age"
+                        id="age"
+                        type="number"
+
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="gender">Gender: </label>
+                    <select
+                        name="gender"
+                        id="gender"
+
+                    >
+                        <option value="m">Male</option>
+                        <option value="f">Female</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label htmlFor="tac">Terms and Conditions:</label>
+                    <input
+                        type="checkbox"
+                        name="tac"
+                        id="tac"
+
+                    />
+                </div>
+
+                <div>
+                    <input type="submit" value="Sign Up" />
+                </div>
+            </form>
+        </div>
+    );
 };
