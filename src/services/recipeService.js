d17ed44e5e83f83fec2ed2c2435937c294f04675
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2023-04-23 20:00:04 +0300

    add services

---
+++ src/services/recipeService.js
@@ -0,0 +1,64 @@
+const baseUrl = 'http://localhost:3030';
+
+// Get data
+
+export const getAll = () => {
+    return fetch(`${baseUrl}/data/recipes`)
+        .then((res) => res.json())
+        .catch((err) => console.log(err));
+};
+
+export const getOne = async (recipeId) => {
+    const response = await fetch(`${baseUrl}/data/recipes/${recipeId}`);
+    const result = await response.json();
+
+    return result.recipe;
+};
+
+// CRUD
+
+export const create = async (recipeData) => {
+    const response = await fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(recipeData),
+    });
+
+    if (response.status == 200) {
+        const result = await response.json();
+        return result;
+    } else {
+        throw new Error('All fields are required!');
+    }
+};
+
+export const edit = async (recipeData, recipeId) => {
+    const response = await fetch(`${baseUrl}/${recipeId}`, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(recipeData),
+    });
+
+    if (response.status == 200) {
+        const result = await response.json();
+        return result;
+    } else {
+        throw new Error('All fields are required!');
+    }
+};
+
+export const del = async (recipeId) => {
+    console.log(recipeId);
+    const response = await fetch(`${baseUrl}/${recipeId}`, {
+        method: 'DELETE',
+        headers: {
+            'content-type': 'application/json',
+        },
+    });
+    const resultMsg = `Deleted: ${response.json()}`;
+    return resultMsg;
+};
