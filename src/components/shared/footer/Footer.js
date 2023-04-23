6cb6ce9a15395b031499ac7ed9821d4927b8caa1
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2023-04-23 13:32:32 +0300

    add HTML for footer and navigation

--- src/components/shared/footer/Footer.js
+++ src/components/shared/footer/Footer.js
@@ -1,3 +1,12 @@
-export const Footer = () => {
-    return <h1>Footer</h1>;
+export const Footer = (props) => {
+    return (
+        <div className="footer-wrapper">
+            <div className="left-side">
+                <h1 className="footer-text">Eat & Amare, your recipe library.</h1>
+            </div>
+            <div className="right-side">
+                <h1 className="footer-text">Enjoy your meal!</h1>
+            </div>
+        </div>
+    );
 };
--- src/components/shared/navigation/Navigation.js
+++ src/components/shared/navigation/Navigation.js
@@ -1,3 +1,53 @@
-export const Navigation = () => {
-    return <h1>Navigation</h1>;
+import { NavLink } from 'react-router-dom';
+// import styles from './Navigation.module.css';
+
+export const Navigation = (props) => {
+    return (
+        <div className="nav-wrapper">
+            <div className="left-side">
+                <div className="nav-link-wrapper">
+                    <NavLink className='' to="/">
+                        Home
+                    </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink className='' to="/recipes/list">
+                        Recipes
+                    </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink className='' to="/about">
+                        About
+                    </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink className='' to="/contacts">
+                        Contacts
+                    </NavLink>
+                </div>
+            </div>
+
+            <div className="right-side">
+                <div className="nav-link-wrapper">
+                    <NavLink to="/">
+                        logout
+                    </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink className='' to="/login">
+                        login
+                    </NavLink>
+                </div>
+                <div className="nav-link-wrapper">
+                    <NavLink className='' to="/register">
+                        try it free
+                    </NavLink>
+                </div>
+            </div>
+        </div>
+    );
 };
